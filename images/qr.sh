a81816aa21feb13939eff296e027b912ce3237df
SanguineSkral <sanguineskralx@gmail.com>
2017-06-14 11:28:34 -0700

    aa

---
+++ images/qr.sh
@@ -0,0 +1,5 @@
+qrencode -o Charlie.png 'http://10.183.2.3';
+qrencode -o downloads.png 'http://10.183.2.3/downloads/';
+qrencode -o tritechsc.github.png 'https://tritechsc.github.io';
+qrencode -o github.png 'https://github.com/tritechsc/';
+qrencode -o cyber.png 'http://tritechsc.org/programs/cyber/';
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>williamp.ioe</title>
+	<!-- No href external links to css or js-->
+	<link rel="stylesheet" href="css/custom.css">
+		<style>
+		#if-location {
+			position: absolute; left:10px; top:230px;
+			width:800px;
+			height:500px;
+			}
+img:hover {
+     opacity: 0.5;
+}
+		</style>
+
+</head>
+<body>
+<!-- add more menue items here -->
+<a href="intro.html"class="menu1" target = "if" >Home</a>&nbsp;
+<a href="N0pe/"class="slink1" >PMA</a>&nbsp;
+<a href="logs/"class="menu1" target = "if" >Logs</a>&nbsp;
+<a href="dev/"class="slink1" target = "blank" >Dev</a>&nbsp;
+<a href="contact.html"class="menu1" target = "if" >Contact</a>&nbsp;
+<a href="html/colors.html"class="slink1" target = "blank" >Colors</a>&nbsp;
+<a href="download/"class="menu1" target = "if" >Downloads</a>&nbsp;
+<a href="http://10.183.2.3:8080/" target = "_blank" class="slink1" >Port 80</a>&nbsp;
+<a href="http://10.183.2.3:3000/" target = "_blank" class="menu1" >Port 30</a>&nbsp;
+<a href="app/" target = "_blank" class="slink1" >PHP-APP</a>&nbsp;
+<a href="JS/rgb.html"class="menu1" target = "if" >RGB</a>&nbsp;
+<a href="https://sanguineskral.github.io/" target = "_blank" class="slink1" >Github</a>&nbsp;
+<img src = "img/colors.png"align="center"height="40px">
+<br />
+<a href="html/" target = "blank" ><img src = "img/HTML.png"height="100"></a>
+<a href="php/" target = "blank" ><img src = "img/PHP button.png"height="100"></a>
+<a href="https://sites.google.com/a/tritechsc.org/williamp/home" target = "blank" ><img src = "img/Google Site.png"height="100"></a>
+<a href="JS/" target = "blank" ><img src = "img/JS.png"height="100"></a>
+<a href="https://github.com/SanguineSkral?tab=repositories" target = "blank" ><img src = "img/Github.png"height="100"></a>
+
+<pre>
+williamp.ioe
+</pre>
+<iframe id="if-location" name="if" src="intro.html" scrolling="yes" frameborder="3">
+</iframe>
+</body>
+</html>
